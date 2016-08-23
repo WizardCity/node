bd389e57248b0f5ad50babf3a46504aaf41b9a2e
WizardCity <twohats9001@gmail.com>
2016-08-23 18:11:14 +0100

    Redone Website project in MEAN stack

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 var express = require('express');
-var path = require('path');
+// var path = require('path');
+// var gulp = require('gulp');
+// var concat = require('gulp-concat');
+// var mongo = require('mongodb');
+// var monk = require('monk');
+// var db = monk('localhost:27017/WebSite');
+
 var app = express();
 
-app.use(express.static('public'));
+//Make our db accessible to our router
+// app.use(function(req,res,next){
+// 	req.db = db;
+// 	next();
+// });
 
-app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
+app.use(express.static(__dirname + '/client'));
+app.use('/js', express.static(__dirname + '/client/js'));
 
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/client/views/index.html');
+});
 
+app.get('/about', function(req, res){
+	res.sendfile(__dirname + '/client/views/about.html');
+});
+
+app.get('/future_projects', function(req, res){
+	res.sendfile(__dirname + '/client/views/future_projects.html');
+});
+
+app.get('/contact', function(req, res){
+	res.sendfile(__dirname + '/client/views/contact.html');
+});
+
+app.get('/login', function(req, res){
+	res.sendfile(__dirname + '/client/views/login.html');
+});
+
+app.get('/register', function(req, res){
+	res.sendfile(__dirname + '/client/views/register.html');
+});
+	
 var server = app.listen(8081, function () {
 
   var host = server.address().address
